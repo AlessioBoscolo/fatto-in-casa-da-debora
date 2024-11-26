deec8b419b22f2865fdfe3851ed39c0262edb289
Alessio Boscolo <73670663+AlessioBoscolo@users.noreply.github.com>
2024-11-26 22:47:17 +0100

    Fixed login and registration of user. Added hashing password

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,85 +1,90 @@
-const pool = require('../config/database');
+const pool = require("../config/database");
+const bcrypt = require("bcrypt");
+
+const saltRounds = 10;
 
 const userController = {
-    registerUser: async (req, res) => {
-        try {
-          const { name, surname, email, password } = req.body;
-          // const saltRounds = 10;
-          
-          // Check if user already exists
-          const userExists = await pool.query(
-            'SELECT * FROM utente WHERE email_utente = $1',
-            [email]
-          );
-      
-          if (userExists.rows.length > 0) {
-            return res.status(400).json({ error: 'User already exists' });
-          }
-      
-          // Hash password
-          // const hash2 = await bcrypt.hash(password, saltRounds);
-          // Insert new user
-          const result = await pool.query(
-            'INSERT INTO utente (nome_utente, cognome_utente, email_utente, password_utente) VALUES ($1, $2, $3, $4) RETURNING id_utente, nome_utente, email_utente',
-            [name, surname, email, password]
-          );
-      
-          res.json(result.rows[0]);
-        } catch (err) {
-          console.error(err);
-          res.status(500).json({ error: err.message });
-        }
-      },
-      
-      // Login endpoint
-      loginUser: async (req, res) => {
-        try {
-          const { email, password } = req.body;
-          
-          console.log(email, password);
-          
-          // Find user
-          const result = await pool.query(
-            'SELECT * FROM utente WHERE email_utente = $1 AND password_utente = $2',
-            [email, password]
-          );
-      
-          if (result.rows.length === 0) {
-            return res.status(401).json({ error: 'Invalid credentials' });
-          }
+  registerUser: async (req, res) => {
+    try {
+      const { name, surname, email, password } = req.body;
 
-      
-          // Check password
-          // const validPassword = await bcrypt.compare(password, result.rows[0].password);
-          // if (!validPassword) {
-          //   return res.status(401).json({ error: 'Invalid credentials' });
-          // }
-      
-          // Don't send password back to client
-          const { password: _, ...user } = result.rows[0];
-          res.json(user);
-        } catch (err) {
-          console.error(err.message);
-          res.status(500).json({ error: 'Server error' });
+      // Check if user already exists
+      const userExists = await pool.query(
+        "SELECT * FROM utente WHERE email_utente = $1",
+        [email]
+      );
+
+      if (userExists.rows.length > 0) {
+        return res.status(400).json({ error: "User already exists" });
+      }
+
+      // var passwordHashed = hashingPassword(password);
+      const hash = await bcrypt.hash(password, saltRounds);
+
+      // Hash password
+      // const hash2 = await bcrypt.hash(password, saltRounds);
+      // Insert new user
+      const result = await pool.query(
+        "INSERT INTO utente (nome_utente, cognome_utente, email_utente, password_utente) VALUES ($1, $2, $3, $4) RETURNING id_utente, nome_utente, email_utente",
+        [name, surname, email, hash]
+      );
+
+      res.json(result.rows[0]);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: err.message });
+    }
+  },
+
+  // Login endpoint
+  loginUser: async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      var validPassword = false;
+
+      // Find user
+      const result = await pool.query(
+        "SELECT * FROM utente WHERE email_utente = $1",
+        [email]
+      );
+
+      if (result.rows.length === 1) {
+        if(result.rows[0].permesso_utente != null){
+          validPassword = await bcrypt.compare(password, result.rows[0].password_utente);
+        }else{
+          return res.status(401).json({ error: "Not yet authorized" });
         }
-      },
-      
-      
-      // Logout endpoint
-      logoutUser: (req, res) => {
-        // If using sessions
-        if (req.session) {
-          req.session.destroy(err => {
-            if (err) {
-              res.status(500).json({ error: 'Could not log out' });
-            } else {
-              res.json({ message: 'Logout successful' });
-            }
-          });
+      } else if (result.rows.length === 0) {
+        return res.status(401).json({ error: "Invalid credentials" });
+      }
+
+      if (!validPassword) {
+        return res.status(401).json({ error: "Invalid credentials" });
+      }
+      // Don't send password back to client
+      const {password_utente: _, ...user } = result.rows[0];
+      res.json(user);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).json({ error: "Server error" });
+    }
+  },
+
+  // Logout endpoint
+  logoutUser: (req, res) => {
+    // If using sessions
+    if (req.session) {
+      req.session.destroy((err) => {
+        if (err) {
+          res.status(500).json({ error: "Could not log out" });
         } else {
-          res.json({ message: 'Logout successful' });
+          res.json({ message: "Logout successful" });
         }
-      },
-}
+      });
+    } else {
+      res.json({ message: "Logout successful" });
+    }
+  },
+};
 
 module.exports = userController;
--- src/pages/UserAccess.jsx
+++ src/pages/UserAccess.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import Image from "../components/Image";
 import Form from "../components/Form/Form";
+import backgroundImage from "../images/backgroundImage.png";
 
 function UserAccess() {
   const isRegistered = window.location.pathname === "/newAccess" ? false : true;
+  const action = isRegistered ? "/home": "/";
 
   return (
     <>
-      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 bg-gray-50">
-        <Image src="./images/logo.png" />
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0 bg-red-500">
         <div className="w-full bg-white rounded-lg md:mt-0 sm:max-w-md xl:p-0">
+          <div className="flex justify-center items-center pt-2">
+            <Image src="./images/logo.png" className="center" />
+          </div>
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
               {isRegistered === true ? "Accedi al tuo account" : "Registrati"}
             </h1>
-            <Form isRegistered={isRegistered} action="/home" method="POST" />
+            <Form isRegistered={isRegistered} action={action} method="POST" />
           </div>
         </div>
       </div>
